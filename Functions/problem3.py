132542381a5f5f6ccd15c18a199c80e7a0cbdd9e
Primal Beast <kheniethforthree3s@gmail.com>
2025-04-15 10:34:19 +0800

    updated files

--- Functions/problem3.py
+++ Functions/problem3.py
@@ -1,2 +1,14 @@
 def Prime_factorization(number):
-    
+    factors = []
+    divided = 2
+
+    while number < 1:
+        while number % divided == 0:
+            factors.append(divided)
+            number //= divided
+        divided =+ 1
+    return factors
+
+input_Num = int(input("Enter a number: "))
+result = Prime_factorization(input_Num)
+print(f"Prime factors:{result}")
--- Functions/problem5.py
+++ Functions/problem5.py
@@ -1,8 +1,28 @@
-def row_Col():
-    for i in range(len(row)):
-        row = int(input(f"Enter a row:{i + 1}"))
+def multiply_matrices(A, B):
+    if len(A[0]) != len(B):
+        return None
 
+    result = []
+    for i in range(len(A)):
+        row = []
+        for j in range(len(B[0])):
+            total = 0
+            for k in range(len(B)):
+                total += A[i][k] * B[k][j]
+            row.append(total)
+        result.append(row)
+    return result
 
+rows_A, cols_A = map(int, input("Enter rows and columns of matrix A: ").split())
+A = [list(map(int, input(f"Row {i+1} of A: ").split())) for i in range(rows_A)]
 
+rows_B, cols_B = map(int, input("Enter rows and columns of matrix B: ").split())
+B = [list(map(int, input(f"Row {i+1} of B: ").split())) for i in range(rows_B)]
 
-"""https://github.com/Sommarfagel398/P-rank.git"""
+result = multiply_matrices(A, B)
+if result is None:
+    print("Incompatible Matrix")
+else:
+    print("Resultant Matrix:")
+    for row in result:
+        print(row)
